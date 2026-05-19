375c922d45979fa3b889ee0cecd6da03c63c1aba
Micah Briggs <r.m.briggs@outlook.com>
2026-05-19 15:35:15 -0500

    feat(frontend): declare VITE_DEV_FAKE_AUTH env var

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string | undefined;
   readonly VITE_SUPABASE_ANON_KEY: string | undefined;
   readonly VITE_API_BASE_URL: string | undefined;
+  readonly VITE_DEV_FAKE_AUTH: string | undefined;
 }
 
 interface ImportMeta {
